6c3537a7643931bc87a874081827431e7a03be88
Murat Uenalan <murat.uenalan@gmail.com>
2023-05-08 17:23:42 +0200

    example1.bash updated

--- examples/example1.bash
+++ examples/example1.bash
@@ -23,7 +23,7 @@ bt_begin 01_notebook1 1
 
   bt_call jupyter nbconvert --to notebook1 --execute Scripts/notebook1.ipynb --output-dir ${DIRPATH_RESULTS}
 
-  bt_fexists ${DIRPATH_RESULTS}/notebook1.ipynb
+  bt_ok_fexists ${DIRPATH_RESULTS}/notebook1.ipynb
 
 
 bt_end
@@ -38,14 +38,14 @@ bt_begin 02_notebook2 2
 
   bt_call jupyter nbconvert --to notebook --execute Scripts/notebook2.ipynb --output-dir ${DIRPATH_RESULTS}
 
-  bt_fexists ${DIRPATH_RESULTS}/notebook2.ipynb
+  bt_ok_fexists ${DIRPATH_RESULTS}/notebook2.ipynb
 
 
   bt_declare Scripts/notebook2_plot.ipynb
 
   bt_call jupyter nbconvert --to notebook --execute Scripts/notebook2_plot.ipynb --output-dir ${DIRPATH_RESULTS}
 
-  bt_fexists ${DIRPATH_RESULTS}/notebook2_plot.ipynb
+  bt_ok_fexists ${DIRPATH_RESULTS}/notebook2_plot.ipynb
 
 
 bt_end
@@ -64,28 +64,28 @@ bt_begin 03_notebook3 4
 
   bt_call dts invoke system/objects/example-dataset/rebuild system/contexts/example-dataset/zebrafish1
 
-  bt_dexists $DTS_DIR_CACHE_HOME/cache/data/example-data/zebrafish1/unified/MATs/mat-algorithm1_local_maxima/
+  bt_ok_dexists $DTS_DIR_CACHE_HOME/cache/data/example-data/zebrafish1/unified/MATs/mat-algorithm1_local_maxima/
 
 
   bt_declare DTS_build_example_data_celegans1
 
   bt_call dts invoke system/objects/example-dataset/rebuild system/contexts/example-dataset/celegans1
 
-  bt_dexists $DTS_DIR_CACHE_HOME/cache/data/example-data/celegans1/unified/MATs/mat-algorithm1_local_maxima/
+  bt_ok_dexists $DTS_DIR_CACHE_HOME/cache/data/example-data/celegans1/unified/MATs/mat-algorithm1_local_maxima/
 
 
   bt_declare DTS_build_example_data_medakashort1
 
   bt_call dts invoke system/objects/example-dataset/rebuild system/contexts/example-dataset/medakashort1
 
-  bt_dexists $DTS_DIR_CACHE_HOME/cache/data/example-data/medakashort1/unified/MATs/mat-algorithm1_local_maxima/
+  bt_ok_dexists $DTS_DIR_CACHE_HOME/cache/data/example-data/medakashort1/unified/MATs/mat-algorithm1_local_maxima/
 
 
   bt_declare DTS_build_example_data_stickleback1
 
   bt_call dts invoke system/objects/example-dataset/rebuild system/contexts/example-dataset/stickleback1
 
-  bt_dexists $DTS_DIR_CACHE_HOME/cache/data/example-data/stickleback1/unified/MATs/mat-algorithm1_local_maxima/
+  bt_ok_dexists $DTS_DIR_CACHE_HOME/cache/data/example-data/stickleback1/unified/MATs/mat-algorithm1_local_maxima/
 
 
 bt_end
